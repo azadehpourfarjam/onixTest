92e941c40d7f6fb4da8c2c1e2eca2ccebb59c120
azadehpourfarjam <azadehpourfarjam@gmail.com>
2025-12-01 23:07:52 +0330

    =first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let mobile=document.querySelector('.mobile');
+let closeBtn=document.querySelector('.close-btn');
+let navBox=document.querySelector('.nav-box');
+let navWrapper=document.querySelector('.nav-wrapper');
+
+//............................................
+let flag=true;
+let projectItem=document.querySelector('.project-des');
+let projectDes=document.querySelector('.project-des');
+// ....................
+$(".card-box").on("click",function (){
+    $(".sub-card").slideToggle();
+});
+
+// .............................
+projectItem.addEventListener('mouseenter',()=>{
+    document.querySelector('.project-item').children[1].classList.remove('project-des-info');
+});
+
+$(".safetyTool-item").hover(function (){
+    $(this).find(".product-view").css('bottom','20px');
+    $(this).find(".safetyTool-pey").css({
+        'opacity':0,
+    });
+},function (){
+    $(this).find(".product-view").css('bottom','-40px');
+    $(this).find(".safetyTool-pey").css({
+        'opacity':1,
+        'display':'flex',
+        'align-items':'center',
+        'justify-content':'space-between'
+    });
+});
+//......................................................................
+mobile.addEventListener('click',function (){
+    navWrapper.style.display='block';
+    setTimeout(function (){
+        navBox.style.right=0
+    },200);
+});
+
+closeBtn.addEventListener('click',function (){
+        navBox.style.right='-1000px';
+        setTimeout(function (){
+            navWrapper.style.display='none';
+            },200)
+
+})
