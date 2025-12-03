c74a3400ff7e9f4a427d2bba17b79ffbe3cda204
azadehpourfarjam <azadehpourfarjam@gmail.com>
2025-12-03 16:07:26 +0330

    =new commit

--- js/main.js
+++ js/main.js
@@ -16,18 +16,12 @@ itemMenu.hover(function(){
 
 
 //............................................
-let flag=true;
-let projectItem=document.querySelector('.project-des');
-let projectDes=document.querySelector('.project-des');
 // ....................
 $(".card-box").on("click",function (){
     $(".sub-card").slideToggle();
 });
 
 // .............................
-projectItem.addEventListener('mouseenter',()=>{
-    document.querySelector('.project-item').children[1].classList.remove('project-des-info');
-});
 
 $(".safetyTool-item").hover(function (){
     $(this).find(".product-view").css('bottom','20px');
@@ -58,3 +52,32 @@ closeBtn.addEventListener('click',function (){
             },200)
 
 })
+
+//.........................................................................
+let flag=true;
+let projectItem=document.querySelector('.project-item');
+$('.project-item').hover(function (){
+    this.children[1].classList.remove('project-des-info');
+    this.children[1].style.left=0;
+    this.children[1].style.width='100%';
+    this.children[1].style.height='100%';
+    this.children[1].innerHTML='';
+    this.children[1].innerHTML=''+'<div class="test">'+
+        '<h4 style="">پروژه ساخت بتن آرمه</h4>'+
+    '<div class="project-des-info1 w-100">' +
+        '<div><span style="display: block;">کارفرما</span><span>امیر محمدی</span></div>'+
+        '<div><span style="display: block;">تاریخ شروع</span><span class="farsiDigit" style="direction: rtl;">03/05/1404</span></div>'+
+        '<div><span style="display: block;">تاریخ پایان</span><span class="farsiDigit">03/05/1404</span></div>'+
+        '</div>'+
+        '</div>'
+    ;
+},
+    function (){
+        this.children[1].classList.add('project-des-info');
+        this.children[1].innerHTML='';
+        this.children[1].style.width='70px';
+        this.children[1].style.width='70px';
+        this.children[1].innerHTML='  <button class="btn btn-red btn-sm"><span class="fa fa-arrow-left"></span></button>\n' +
+            '                            <h3 class="">پروژه ساخت بلن آرمه</h3>';
+}
+    );
